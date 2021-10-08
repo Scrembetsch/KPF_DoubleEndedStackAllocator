27b0b56bfc00cc30c8c9500bcac372ddc61d556d
incredibleLeitman <michael.leithner@gmail.com>
2021-10-08 14:52:12 +0200

    Gruppenaufteilung und basic issues

--- KPF_DoubleEndedStackAllocator/src/main_skeleton.cpp
+++ KPF_DoubleEndedStackAllocator/src/main_skeleton.cpp
@@ -1,11 +1,21 @@
 /**
 * Exercise: "DoubleEndedStackAllocator with Canaries" OR "Growing DoubleEndedStackAllocator with Canaries (VMEM)"
-* Group members: Handls Anja (gs20m005), Tributsch Harald (gsgs20m008), Leithner Michael (gs20m012)
+* Group members: Handl Anja (gs20m005), Tributsch Harald (gs20m008), Leithner Michael (gs20m012)
+* 
+* open issues:
+* ============
+* - virtual memory allocation for growing VMEM
+*		Michael
+* - allocate, align
+*		Harald
+* - free, reset
+*		Anja
 **/
 
 #include "stdio.h"
 
 #include <cassert>
+#include <cstdint>
 
 namespace Tests
 {
@@ -68,11 +78,17 @@ namespace Tests
 class DoubleEndedStackAllocator
 {
 public:
-	DoubleEndedStackAllocator(size_t max_size) {}
+	DoubleEndedStackAllocator(size_t max_size)
+	{
+		// TODO: reserve memory (alloc, VirtualAlloc)
+	}
 
 	void* Allocate(size_t size, size_t alignment)
 	{
 		assert(IsPowerOf2(alignment));
+		// TODO: return nullptr if failed
+
+		// TODO: align
 
 		return nullptr;
 	}
@@ -81,18 +97,80 @@ public:
 		return nullptr;
 	}
 
-	void Free(void* memory) {}
+	void Free(void* memory)
+	{
+		// TODO: check if pointer is valid?
+
+		// TODO: reposition pointer and free memory
+	}
+
 	void FreeBack(void* memory) {}
 
-	void Reset(void) {}
+	void Reset(void)
+	{
+		// TODO: while Free()
+	}
 
-	~DoubleEndedStackAllocator(void) {}
+	~DoubleEndedStackAllocator(void)
+	{
+		// TODO: give reserved memory back to system
+	}
 
 private:
 	// power of 2 always has exactly 1 bit set in binary representation (for signed values)
 	bool IsPowerOf2(size_t val)
 	{
 		return val > 0 && !(val & (val - 1));
+	}
+
+#ifdef WITH_DEBUG_CANARIES
+	const ptrdiff_t CANARY_SIZE = 4;
+#else
+	const ptrdiff_t CANARY_SIZE = 0;
+#endif
+
+	const ptrdiff_t META_SIZE = 4; // contains allocated space size
+	const uint32_t CANARY = 0xDEADC0DE;
+
+	//					|	|	|	|
+	//					4	8	12	16
+	//			[0xCD]<meta>Front
+
+	// [Begin... [0xCD] <meta>Front, <meta>Front......, <meta>Back, [0xCD] ... End] <- memalloc (VirtualAlloc)
+
+	// first reservation 
+	// [Begin...															... End]
+
+	// allocate
+	// [Begin, [0xCD], <meta>Front, [0xCD]									... End]
+
+	// allocate
+	// [Begin, [0xCD], <meta>Front, [0xCD], ... [0xCD], <meta>Front, [0xCD]	... End]
+
+	// free
+	// [Begin, [0xCD], <meta>Front, [0xCD], ...								... End]
+	//									    ^
+	
+	// allocateBack
+	// [Begin, [0xCD], <meta>Front, [0xCD], ... [0xCD], <meta>Back, [0xCD]	... End]
+
+	// boundaries of our allocation
+	uintptr_t mBegin;
+	uintptr_t mEnd;
+
+	// decision: (A) using pointer to next/prev free memory or (B) points to user space begin
+	// --> (B)
+	uintptr_t mFront;
+	uintptr_t mBack;
+
+	uintptr_t AlignUp()
+	{
+		return 0x0;
+	}
+
+	uintptr_t AlignDown()
+	{
+		return 0x0;
 	}
 };
 
@@ -125,10 +203,12 @@ int main()
 			allocator.Allocate(0, 2);
 
 			// fail
-			//printf("allocator.Allocate(0, 0)...\n");
-			//allocator.Allocate(0, 0);
-			//printf("allocator.Allocate(0, 3)...\n");
-			//allocator.Allocate(0, 3);
+			#ifndef _DEBUG
+				printf("allocator.Allocate(0, 0)...\n");
+				allocator.Allocate(0, 0);
+				printf("allocator.Allocate(0, 3)...\n");
+				allocator.Allocate(0, 3);
+			#endif
 		}
 
 	}
